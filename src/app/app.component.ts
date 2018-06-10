f0fc98142abc466a828b20d154bf6fad6c331525
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-06-10 01:28:44 -0300

    Criando menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  opened: boolean = true;
+  openMenu()
+  {
+    this.opened = !this.opened;
+  }
 }
