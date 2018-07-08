2daaa610f0fc2e9afc73eaf2fcfa394b64452c44
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-07-08 12:14:36 -0300

    Exibindo dados do formulario no console

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, FormArray, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,18 +11,45 @@ export class SignupComponent implements OnInit {
 
   constructor(
     private router: Router,
+    private formBuilder: FormBuilder,
   ) { }
 
   formGroup: FormGroup;
+  zipcode: string;
 
   ngOnInit() 
   {
+    this.initForm();
+  };
 
+  initForm()
+  {
+    this.formGroup = this.formBuilder.group({
+      email: [null, [Validators.required]],
+      name: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      password_confirmation: [null, [Validators.required]],
+      phone: [null, [Validators.required]],
+      address: [null, [Validators.required]],
+      city: [null, [Validators.required]],
+      state: [null, [Validators.required]],
+      zipCode: [null, [Validators.required]],
+    });
   };
 
   back()
   {
     this.router.navigate(['login']);
+  };
+
+  getZipcode()
+  {
+    console.log(this.zipcode)
+  }
+
+  login()
+  {
+    console.log(this.formGroup.value)
   };
 
 }
