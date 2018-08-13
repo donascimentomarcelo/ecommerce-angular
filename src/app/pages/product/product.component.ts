e8d2ce72f09c3430f66b03eeca93971cffc10276
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-08-12 21:51:21 -0300

    Ajustes lint em produtos

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -18,10 +18,10 @@ export class ProductComponent implements OnInit {
   total: number;
   formGroup: FormGroup;
   idSelected: number = null;
-  searchValue: string = '';
-  findByName: boolean =  true;
-  findById: boolean =  false;
-  findByCatName: boolean =  false;
+  searchValue = '';
+  findByName =  true;
+  findById =  false;
+  findByCatName =  false;
   searchPlaceholder: string;
 
   constructor(
@@ -30,84 +30,74 @@ export class ProductComponent implements OnInit {
     private formBuilder: FormBuilder
   ) { }
 
-  ngOnInit() 
-  {
+  ngOnInit() {
     this.searchPlaceholder = 'nome';
     this.loadProducts(0);
     this.loadCategories();
     this.initForm();
-  };
+  }
 
-  initForm()
-  {
+  initForm() {
     this.formGroup = this.formBuilder.group({
       id: [null],
       name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(80)]],
-      category_id: [null, [Validators.required,]],
+      category_id: [null, [Validators.required]],
       price: [null, [Validators.required,  Validators.minLength(1), Validators.maxLength(10)]],
       description: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(80)]]
     });
-  };
+  }
 
-  loadProducts(page: number)
-  {
+  loadProducts(page: number) {
     this.productService.list(page)
       .subscribe(response => {
-        this.products = response['data'];   
-        this.total = response['total'];   
+        this.products = response['data'];
+        this.total = response['total'];
       }, error => {
 
       });
-  };
+  }
 
-  loadCategories()
-  {
+  loadCategories() {
     this.categoryService.list()
       .subscribe(response => {
         this.categories = response;
       }, error => {
 
-      })
-  };
+      });
+  }
 
-  save()
-  {
-    if(this.formGroup.value.id)
-    {
+  save() {
+    if (this.formGroup.value.id) {
       this.update(this.formGroup.value, this.formGroup.value.id);
-    }
-    else
-    {
+    }  else {
       this.create(this.formGroup.value);
-    };
-  };
+    }
+  }
 
-  create(product: ProductDTO)
-  {
+  create(product: ProductDTO) {
     this.productService.create(product)
       .subscribe(response => {
-        let prod: any = {
-            id: response['id'], 
+        const prod: any = {
+            id: response['id'],
             name: response['name'],
             price: response['price'],
             description: response['description'],
             category: response['category'],
             category_id: response['category_id']
-          }
-        this.products.push(prod)
+          };
+        this.products.push(prod);
       }, error => {
 
       });
-  };
+  }
 
-  update(product: ProductDTO, id: number)
-  {
+  update(product: ProductDTO, id: number) {
     this.productService.update(product, id)
       .subscribe(response => {
-        
-        let updatedProduct = this.products.find(this.findIndexToUpdate, product.id);
 
-        let index = this.products.indexOf(updatedProduct);
+        const updatedProduct = this.products.find(this.findIndexToUpdate, product.id);
+
+        const index = this.products.indexOf(updatedProduct);
 
         this.products[index] = response as ProductDTO;
 
@@ -116,50 +106,44 @@ export class ProductComponent implements OnInit {
       }, error => {
 
       });
-  };
+  }
 
-  findIndexToUpdate(item)
-  {
+  findIndexToUpdate(item) {
     return item.id === this;
-  };
+  }
 
-  edit(product: ProductDTO)
-  {
+  edit(product: ProductDTO) {
     this.idSelected = product.id;
 
     this.formGroup = this.formBuilder.group({
       id: [product.id],
       name: [product.name, [Validators.required, Validators.minLength(3), Validators.maxLength(80)]],
-      category_id: [product.category_id, [Validators.required,]],
+      category_id: [product.category_id, [Validators.required]],
       price: [product.price, [Validators.required,  Validators.minLength(1), Validators.maxLength(10)]],
       description: [product.description, [Validators.required, Validators.minLength(3), Validators.maxLength(80)]]
     });
-  };
+  }
 
-  clear()
-  {
+  clear() {
     this.idSelected = null;
     this.formGroup.reset();
-  };
-  
-  search(event: any)
-  {
-    if(event.target.value === '' || event.target.value === undefined)
-    {
+  }
+
+  search(event: any) {
+    if (event.target.value === '' || event.target.value === undefined) {
       this.searchValue = '';
       this.loadProducts(0);
       return;
-    };
+    }
 
-    const val: any = event.target.value; 
+    const val: any = event.target.value;
 
-    switch(this.searchPlaceholder)
-    {
+    switch (this.searchPlaceholder) {
 
       case 'id':
       this.findProductById(val);
       break;
-      
+
       case 'nome':
       this.findProductByName(val);
       break;
@@ -167,63 +151,57 @@ export class ProductComponent implements OnInit {
       case 'nome da categoria':
       this.findByCategoryName(val);
       break;
-    };
-  };
-  
-  selectFindById()
-  {
+    }
+  }
+
+  selectFindById() {
     this.findByName =  false;
     this.findById = true;
     this.findByCatName = false;
     this.searchPlaceholder = 'id';
-  };
+  }
 
-  selectFindByName()
-  {
+  selectFindByName() {
     this.findByName = true;
     this.findById = false;
     this.findByCatName = false;
     this.searchPlaceholder = 'nome';
-  };
+  }
 
-  selectFindByCategoryName()
-  {
+  selectFindByCategoryName() {
     this.findByName = false;
     this.findById = false;
     this.findByCatName = true;
     this.searchPlaceholder = 'nome da categoria';
-  };
+  }
 
-  findProductById(id: number)
-  {
+  findProductById(id: number) {
     this.productService.findOne(id)
       .subscribe(response => {
-        let value: any = [];
+        const value: any = [];
         value.push(response);
         this.products = value;
       }, error => {
 
       });
-  };
+  }
 
-  findProductByName(name: string)
-  {
+  findProductByName(name: string) {
     this.productService.findByName(name)
       .subscribe(response => {
         this.products = response;
       }, error => {
 
       });
-  };
+  }
 
-  findByCategoryName(name: string)
-  {
+  findByCategoryName(name: string) {
     this.productService.findByCategoryName(name)
       .subscribe(response => {
         this.products = response;
       }, error => {
 
       });
-  };
+  }
 
 }
